c3954844671d76d317cfe90f630641056a0d7506
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-22 10:19:19 -0300

    (chore) Centralizando as URLs das APIs em uma classe dentro de configuration/

--- app/src/main/java/com/purpura/app/configuration/ApiEndpoints.java
+++ app/src/main/java/com/purpura/app/configuration/ApiEndpoints.java
@@ -1,7 +1,7 @@
 package com.purpura.app.configuration;
 
 public class ApiEndpoints {
-    public static final String MONGO_URL = "https://mongodb-api-purpura.onrender.com";
-    public static final String POSTGRES_URL = "https://api-pg-purpura-latest.onrender.com/";
-    public static final String MICRO_URL = "https://micro-api-purpura.onrender.com";
+    public static final String MONGO_URL = "https://api-mg-purpura.onrender.com";
+    public static final String POSTGRES_URL = "https://api-pg-purpura.onrender.com/alive";
+    public static final String MICRO_URL = "https://api-micro-purpura.onrender.com";
 }
