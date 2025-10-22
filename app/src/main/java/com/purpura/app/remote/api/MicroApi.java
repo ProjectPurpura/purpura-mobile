b0225fa3b0a061af49f51b751cef7e344a5b85da
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-22 11:14:28 -0300

    (feat) Integração com microserviço de CEP

---
+++ app/src/main/java/com/purpura/app/remote/api/MicroApi.java
@@ -0,0 +1,15 @@
+package com.purpura.app.remote.api;
+
+import com.purpura.app.configuration.EnvironmentVariables;
+import com.purpura.app.model.micro.CepResponse;
+import com.purpura.app.remote.util.Api;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+@Api(EnvironmentVariables.MICRO_URL)
+public interface MicroApi {
+    @GET("/cep/{cep}")
+    Call<CepResponse> getCep(@Path("cep") String cep);
+}
