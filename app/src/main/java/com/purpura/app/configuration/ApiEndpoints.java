fb979ebcca3232d4a0c73acc2616d9d06de5ce1f
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-22 10:39:21 -0300

    (remove) Removendo o ApiEndpoints.java

--- app/src/main/java/com/purpura/app/configuration/ApiEndpoints.java
+++
@@ -1,7 +0,0 @@
-package com.purpura.app.configuration;
-
-public class ApiEndpoints {
-    public static final String MONGO_URL = "https://api-mg-purpura.onrender.com";
-    public static final String POSTGRES_URL = "https://api-pg-purpura.onrender.com/alive";
-    public static final String MICRO_URL = "https://api-micro-purpura.onrender.com";
-}
