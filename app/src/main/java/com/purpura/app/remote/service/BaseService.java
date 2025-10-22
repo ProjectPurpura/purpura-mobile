b72f7fcbb4551b6b4a23ab9c05db1ca894973966
rkhue <85773727+rkhue@users.noreply.github.com>
2025-10-22 11:42:19 -0300

    (bugfix) Fazendo o atributo da classe abstrata BaseService ser protected

--- app/src/main/java/com/purpura/app/remote/service/BaseService.java
+++ app/src/main/java/com/purpura/app/remote/service/BaseService.java
@@ -3,7 +3,7 @@ package com.purpura.app.remote.service;
 import com.purpura.app.remote.util.RetrofitService;
 
 public abstract class BaseService<T> {
-    private T api;
+    protected T api;
 
     public BaseService(Class<T> apiClass) {
         this.api = new RetrofitService<>(apiClass).getService();
